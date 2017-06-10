f3b4aedda36079c52da0a5431f5026f221de2b82
cbaddeley <corybaddeley@gmail.com>
2017-06-10 19:16:51 -0400

    some stuff

---
+++ assets/javascript/game.js
@@ -0,0 +1,106 @@
+var blackMage = {
+		attack : 5,
+		health : 50,
+		counter : 5,
+		imgLeft : "assets/images/blackMageLeft.png",
+		imgRight: "assets/images/blackMageRight.png",
+		selectDiv : "#black"
+	};
+var knight = {
+		attack : 5,
+		health : 50,
+		counter : 5,
+		imgLeft : "assets/images/knightLeft.png",
+		imgRight: "assets/images/knightRight.png",
+		selectDiv : "#knight"
+	};
+var blueMage = {
+		attack : 5,
+		health : 50,
+		counter : 5,
+		imgLeft : "assets/images/blueMageLeft.png",
+		imgRight: "assets/images/blueMageRight.png",
+		selectDiv : "#blue"
+	};
+var dragoon = {
+		attack : 5,
+		health : 50,
+		counter : 5,
+		imgLeft : "assets/images/dragoonLeft.png",
+		imgRight: "assets/images/dragoonRight.png",
+		selectDiv : "#dragoon"
+	};
+var characters = [blackMage, knight, blueMage, dragoon];
+var characterSelect = false;
+var oSelect = false;
+var addAttack = 0;
+var defHealth = 0;
+var attHealth = 0;
+var initial = false;
+var audio = $("#hover")[0];
+	
+function charSelect(selectedCharacter)  {
+	
+	if (!characterSelect) {
+		console.log(selectedCharacter);
+		console.log(selectedCharacter.imgRight);
+		console.log($(selectedCharacter).attr("arrRef"));
+		$(selectedCharacter).attr("src", characters[$(selectedCharacter).attr("arrRef")].imgRight);
+		$(selectedCharacter).detach().prependTo($("#attackerArea"));
+		$("#charSelectScreen > :input").detach().prependTo($("#opponentArea"));
+		characterSelect = true;
+		$("#charSelectScreen").hide();
+		audio = null;
+	}
+		
+	if (oSelect == false && $(selectedCharacter).parent().is("#opponentArea")) {
+		$(selectedCharacter).detach().prependTo($("#defenderArea"));
+		oSelect = true;
+		defHealth = characters[$("#defenderArea > :input").attr("arrRef")].health;
+	}
+	
+}
+
+// function selectOpponent() {
+// 	$(":input").click(function() {
+		
+// 	});
+// }
+
+function attack() {
+	if (initial == false) {
+		addAttack = characters[$("#attackerArea > :input").attr("arrRef")].attack;
+		attHealth = characters[$("#attackerArea > :input").attr("arrRef")].health;
+		initial = true;
+		console.log(addAttack);
+		console.log(defHealth);
+		console.log(attHealth);
+	} 
+
+	if (defHealth > 0) {
+		defHealth = defHealth - addAttack;
+		addAttack = addAttack + characters[$("#attackerArea > :input").attr("arrRef")].attack;
+		console.log(defHealth);
+		console.log(addAttack);
+	}
+	if (defHealth > 0) {
+		attHealth = attHealth - characters[$("#defenderArea > :input").attr("arrRef")].counter;
+		console.log(attHealth);
+	}	else {
+		oSelect = false;
+		$("#defenderArea > :input").remove();
+		
+
+	}
+}
+
+for (var i = 0; i < characters.length; i++) {
+	$(characters[i].selectDiv).click(function() {
+		charSelect(this);
+})}
+
+
+$("#charSelectScreen > :input").mouseenter(function() {
+  audio.play();
+});
+
