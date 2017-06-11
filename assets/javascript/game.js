8e5c1f3330d20fb5dbbf333365f86e2726a038f0
cbaddeley <corybaddeley@gmail.com>
2017-06-11 01:15:46 -0400

    everything looks functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,44 @@
 var blackMage = {
-		attack : 5,
-		health : 50,
-		counter : 5,
+		attack : 16,
+		health : 240,
+		counter : 30,
 		imgLeft : "assets/images/blackMageLeft.png",
 		imgRight: "assets/images/blackMageRight.png",
+		imgDeadLeft : "assets/images/blackMageDeadLeft.png",
+		imgDeadRight : "assets/images/blackMageDeadRight.png",
 		selectDiv : "#black",
 		name: "Black Mage"
 	};
 var knight = {
-		attack : 5,
-		health : 50,
-		counter : 5,
+		attack : 28,
+		health : 200,
+		counter : 20,
 		imgLeft : "assets/images/knightLeft.png",
 		imgRight: "assets/images/knightRight.png",
+		imgDeadLeft : "assets/images/knightDeadLeft.png",
+		imgDeadRight : "assets/images/knightDeadRight.png",
 		selectDiv : "#knight",
 		name: "Knight"
 	};
 var blueMage = {
-		attack : 5,
-		health : 50,
-		counter : 5,
+		attack : 16,
+		health : 260,
+		counter : 50,
 		imgLeft : "assets/images/blueMageLeft.png",
 		imgRight: "assets/images/blueMageRight.png",
+		imgDeadLeft : "assets/images/blueMageDeadLeft.png",
+		imgDeadRight : "assets/images/blueMageDeadRight.png",
 		selectDiv : "#blue",
 		name: "Blue Mage"
 	};
 var dragoon = {
-		attack : 5,
-		health : 50,
-		counter : 5,
+		attack : 16,
+		health : 300,
+		counter : 45,
 		imgLeft : "assets/images/dragoonLeft.png",
 		imgRight: "assets/images/dragoonRight.png",
+		imgDeadLeft : "assets/images/dragoonDeadLeft.png",
+		imgDeadRight : "assets/images/dragoonDeadRight.png",
 		selectDiv : "#dragoon",
 		name: "Dragoon"
 	};
@@ -42,13 +50,15 @@ var defHealth = 0;
 var attHealth = 0;
 var initial = false;
 var audio = $("#hover")[0];
+var victorySound = $("#victorySound")[0];
+var gameOverSound = $("#gameOverSound")[0];
+var music = $("#musicSound")[0];
+var attackSound = $("#attackSound")[0];
+var wins = 0;
 	
 function charSelect(selectedCharacter)  {
 	
 	if (!characterSelect) {
-		console.log(selectedCharacter);
-		console.log(selectedCharacter.imgRight);
-		console.log($(selectedCharacter).attr("arrRef"));
 		$(".loadUp").css("display","block");
 		$(selectedCharacter).attr("src", characters[$(selectedCharacter).attr("arrRef")].imgRight);
 		$("#attackerName").text(characters[$(selectedCharacter).attr("arrRef")].name);
@@ -61,6 +71,7 @@ function charSelect(selectedCharacter)  {
 	}
 		
 	if (oSelect == false && $(selectedCharacter).parent().is("#opponentArea")) {
+		$("#defenderArea > :input").remove();
 		$(selectedCharacter).detach().prependTo($("#defenderArea"));
 		$("#defenderName").text(characters[$(selectedCharacter).attr("arrRef")].name);
 		$("#defenderHealth").text("HP: " + characters[$(selectedCharacter).attr('arrRef')].health + " / " + characters[$(selectedCharacter).attr('arrRef')].health);
@@ -82,20 +93,15 @@ function attack() {
 		addAttack = characters[$("#attackerArea > :input").attr("arrRef")].attack;
 		attHealth = characters[$("#attackerArea > :input").attr("arrRef")].health;
 		initial = true;
-		console.log(addAttack);
-		console.log(defHealth);
-		console.log(attHealth);
 	} 
-
 	if (defHealth > 0) {
+		attackSound.play();
 		defHealth = defHealth - addAttack;
 		addAttack = addAttack + characters[$("#attackerArea > :input").attr("arrRef")].attack;
 		$("#defenderHealth").text("HP: " + defHealth + " / " + characters[$("#defenderArea > :input").attr("arrRef")].health);
 		$("#defHealthPerct").width(function(n, c){
         return (defHealth / characters[$("#defenderArea > :input").attr("arrRef")].health) * 100;
         });
-		console.log(defHealth);
-		console.log(addAttack);
 	}
 	if (defHealth > 0) {
 		attHealth = attHealth - characters[$("#defenderArea > :input").attr("arrRef")].counter;
@@ -103,16 +109,41 @@ function attack() {
 		$("#attHealthPerct").width(function(n, c){
         return (attHealth / characters[$("#attackerArea > :input").attr("arrRef")].health) * 100;
         });
-		console.log(attHealth);
-	}	else {
+	} 
+	if (defHealth <= 0 && oSelect) {
 		oSelect = false;
 		$("#defenderName").text("");
 		$("#defenderHealth").text("");
 		$("#healthBarDef").css("display","none");
+		wins++;
 		$("#defHealthPerct").width(function(n, c){
         return (100 / 100) * 100;
         });
-		$("#defenderArea > :input").remove();
+		$("#defenderArea > :input").attr("src", characters[$("#defenderArea > :input").attr("arrRef")].imgDeadLeft);
+		}
+	if (attHealth < 1) {
+		$("#attackerArea > :input").attr("src", characters[$("#attackerArea > :input").attr("arrRef")].imgDeadRight);
+		$("#attackerArea > :input").animate({ height: "400px", width: "400px"});
+		$("#instructions").animate({ opacity: "0" });
+		$(":button").css("display", "none");
+		$("#statsArea").animate({ opacity: "0" });
+		$("#defenderArea").animate({ opacity: "0" });
+		$("#opponentArea").animate({ opacity: "0" });
+		$("#gameOver").css("display", "block");
+		$("#gameOver").animate({ left: "+=1100px" }, "slow");
+		music.pause();
+		gameOverSound.play();
+	}
+	if (wins == 3) {
+		$("#attackerArea > :input").animate({ height: "400px", width: "400px"});
+		$("#instructions").animate({ opacity: "0" });
+		$(":button").css("display", "none");
+		$("#defenderArea").animate({ opacity: "0" });
+		$("#statsArea").animate({ opacity: "0" });
+		$("#victory").css("display", "block");
+		$("#victory").animate({ left: "+=1240px" }, "slow");
+		music.pause();
+		victorySound.play();
 	}
 }
 
@@ -121,8 +152,13 @@ for (var i = 0; i < characters.length; i++) {
 		charSelect(this);
 })}
 
+	$("#charSelectScreen > :input").mouseenter(function() {
+ 		if (!characterSelect) {
+ 		audio.play();
+ 		}
+	});
 
-$("#charSelectScreen > :input").mouseenter(function() {
-  audio.play();
+$(document).ready(function() {
+    music.play();
 });
 
